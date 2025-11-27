709cfc9f075f36727277732029c1c859aaec5b2a
santi <santisalgado200@utlook.com>
2025-11-27 17:25:17 -0500

    traer datos

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hola', function () {
+    return "Hola, Laravel";
+});
+
+Route::get('/hola', function () {
+    $nombre= "Santiago";
+    return "Hola, Laravel {$nombre}";
+});
+
+Route::get('/cupon', function () {
+    $cupon = DB::table('cupon')->get();
+    return response()->json($cupon, 200, [], JSON_UNESCAPED_UNICODE);
+});
