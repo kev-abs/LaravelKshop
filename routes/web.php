48324737ff150d1411e99ef80973c5f931aba27c
Manuel <manuel25142808@gmail.com>
2025-11-27 17:28:03 -0500

    Se pueden ver los get

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+
+Route::get('/prueba-db', function () {
+    try {
+        DB::connection()->getPdo();
+        return "Conectado correctamente a la base de datos: " . DB::connection()->getDatabaseName();
+    } catch (\Exception $e) {
+        return "Error al conectar: " . $e->getMessage();
+    }
+});
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hola', function () {
+    return "Hola, Laravel";
+});
+Route::get('/clientes', function () {
+    $clientes = DB::table('cliente')->get();
+
+    return response()->json($clientes, 200, [], JSON_UNESCAPED_UNICODE);
+});
+
